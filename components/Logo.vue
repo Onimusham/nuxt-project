e7d59b8ac67a05adb646102dbb97af242398fd5f
Onimusham <viniciusluviah@gmail.com>
2021-04-10 17:15:49 -0300

    Teste2

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="-m-24 sm:-m-10 m lg:m-32">
     <div class='flex pb-40 md:pb-12 lg:pb-52'>
-      <img class="mx-auto w-60" src="@/assets/Tita-drinking.png" />
+      <img class="mx-auto w-65" src="@/assets/Tita-drinking.png" />
     </div>
     <div class='font-cantarell text-2xl lg:text-3xl subpixel-antialiased pt-5 cursor-default text-gray-800'>
       <div class="flex flex-row py-1.5 mx-7">
